a6c55370857440b7097a165cdcce3df504cd40ec
Asvel <fe.asvel@gmail.com>
2021-03-10 04:34:20 +0800

    Add tests.

---
+++ src/__test__/Weather.test.ts
@@ -0,0 +1,25 @@
+import * as W from '../Weather';
+
+Date.now = () => 1609459200000;
+W.init();
+
+it('calculates weathers correctly', () => {
+  expect(W.state.weathers).toMatchSnapshot();
+});
+
+it('finds list correctly', () => {
+  expect(W.find({
+    zone: 'Limsa Lominsa',
+    hourMask: { 0: true, 8: true, 16: true },
+  }).map(m => m())).toMatchSnapshot();
+});
+
+it('finds condition correctly', () => {
+  expect(W.find({
+    zone: 'Limsa Lominsa',
+    desiredWeathers: [0, 3],
+    previousWeathers: [4],
+    beginHour: 7,
+    endHour: 14,
+  }).map(m => m())).toMatchSnapshot();
+});
