00f8642b8ee0b74ed15afc622101c095c5c4a9b7
Asvel <fe.asvel@gmail.com>
2024-07-25 15:54:54 +0800

    Fix timezone handling of event mode.

--- src/views/EventMain.tsx
+++ src/views/EventMain.tsx
@@ -1,19 +1,20 @@
+import { createMemo } from 'solid-js';
 import strftime = require('strftime');
 import * as W from '../Weather';
 import { t, getCurrentLanguage } from '../i18n';
+import { indexRender } from '../utils';
 import { FriendlyTime } from './FriendlyTime';
 import { Footer } from './Footer';
 
 export function EventMain({ eventId }: { eventId: W.EventId }) {  // eslint-disable-line solid/no-destructure
   const event = W.events[eventId];
-  const matches = event.matcher();
-  const forceUtc8 = getCurrentLanguage() === 'zh';
-  const strftimeT = forceUtc8 ? strftime.timezone(+480) : strftime;
+  const matches = event.matcher().slice(0, 20);
+  const forceUtc8 = () => getCurrentLanguage() === 'zh';
   return (
     <div class="app">
       <div class="console clearfix">
         <span class="console_summary">{t(event.description)}</span>
-        {forceUtc8 && <span class="console_timezone">UTC+08:00</span>}
+        {forceUtc8() && <span class="console_timezone">UTC+08:00</span>}
       </div>
       <div class="match">
         <table>
@@ -24,15 +25,17 @@ export function EventMain({ eventId }: { eventId: W.EventId }) {  // eslint-disa
           </tr>
           </thead>
           <tbody>
-          {/*@once*/matches.slice(0, 20).map(({ begin, end }) => (  // eslint-disable-line solid/no-destructure
+          {indexRender(createMemo(() => forceUtc8()
+            ? matches.map(m => ({ begin: shiftDateAsUtc8(m.begin), end: shiftDateAsUtc8(m.end) }))
+            : matches), m => (
             <tr>
               <td class="match_event-start-time">
-                <span class="match_local-time-date">{/*@once*/strftimeT('%m/%d ', begin)}</span>
-                <FriendlyTime date={begin} />
+                <span class="match_local-time-date">{strftime('%m/%d ', m().begin)}</span>
+                <FriendlyTime date={m().begin} />
               </td>
               <td class="match_event-end-time">
-                <span class="match_local-time-date">{/*@once*/strftimeT('%m/%d ', end)}</span>
-                <FriendlyTime date={end} />
+                <span class="match_local-time-date">{strftime('%m/%d ', m().end)}</span>
+                <FriendlyTime date={m().end} />
               </td>
             </tr>
           ))}
@@ -42,4 +45,11 @@ export function EventMain({ eventId }: { eventId: W.EventId }) {  // eslint-disa
       <Footer />
     </div>
   );
+}
+
+function shiftDateAsUtc8(date: Date) {
+  const timestamp = date.getTime();
+  const shifted = new Date(timestamp + (date.getTimezoneOffset() + 480) * 60000);
+  shifted.setTime(timestamp + (shifted.getTimezoneOffset() + 480) * 60000);  // might cross DST boundary
+  return shifted;
 }
