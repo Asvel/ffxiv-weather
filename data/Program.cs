f503962e6d153744cca546a81e0b9746193083bb
Asvel <fe.asvel@gmail.com>
2024-11-17 03:47:21 +0800

    Upgrade Lumina.

--- data/Program.cs
+++ data/Program.cs
@@ -2,13 +2,13 @@ using System.Collections.Specialized;
 using System.Text;
 using Lumina;
 using Lumina.Excel;
-using Lumina.Excel.GeneratedSheets;
-using Lumina.Text;
+using Lumina.Excel.Sheets;
+using Lumina.Text.ReadOnly;
 
 var lumina = new GameData(args[0], new() { DefaultExcelLanguage = Lumina.Data.Language.English });
 var luminaCn = new GameData(args[1], new() { DefaultExcelLanguage = Lumina.Data.Language.ChineseSimplified });
 
-var escape = (SeString? s) => s?.RawString?.Replace("'", "\\'");
+var escape = (ReadOnlySeString? s) => s?.ExtractText()?.Replace("'", "\\'");
 
 var weathers = new OrderedDictionary();
 var placeUsed = new HashSet<uint>();
@@ -18,24 +18,25 @@ var weatherUsed = new HashSet<uint>();
     var weatherRateSheet = lumina.GetExcelSheet<WeatherRate>()!;
     var weatherSheet = lumina.GetExcelSheet<Weather>()!;
     var weatherReportReplaceSheet = lumina.GetExcelSheet<WeatherReportReplace>()!;
-    var weatherReportReplaces = weatherReportReplaceSheet.ToDictionary(x => x.PlaceNameSub.Row, x => x.PlaceNameParent);
+    var weatherReportReplaces = weatherReportReplaceSheet.ToDictionary(x => x.PlaceNameSub.RowId, x => x.PlaceNameParent);
     foreach (var territoryType in territoryTypeSheet)
     {
         var weatherRate = weatherRateSheet.GetRow(territoryType.WeatherRate)!;
-        if (weatherRate.UnkData0[0].Rate == 100) continue;
-        weatherReportReplaces.TryGetValue(territoryType.PlaceName.Row, out var placeName);
-        placeName ??= territoryType.PlaceName;
-        placeUsed.Add(placeName.Row);
-        var line = new StringBuilder();
-        line.Append("  '"); line.Append(escape(placeName.Value?.Name) ?? ""); line.Append("': [");
-        var rate = 0;
-        foreach (var entry in weatherRate.UnkData0)
+        if (weatherRate.Rate[0] == 100) continue;
+        if (!weatherReportReplaces.TryGetValue(territoryType.PlaceName.RowId, out var placeName))
         {
-            if (entry.Weather == 0) break;
-            var weather = weatherSheet.GetRow((uint)entry.Weather)!;
+            placeName = territoryType.PlaceName;
+        }
+        placeUsed.Add(placeName.RowId);
+        var line = new StringBuilder();
+        line.Append("  '"); line.Append(escape(placeName.ValueNullable?.Name) ?? ""); line.Append("': [");
+        var rate = 0;
+        foreach (var (weather, rate_) in weatherRate.Weather.Zip(weatherRate.Rate))
+        {
+            if (weather.RowId == 0) break;
             weatherUsed.Add(weather.RowId);
-            rate += entry.Rate;
-            line.Append('\''); line.Append(escape(weather.Name)); line.Append('\'');
+            rate += rate_;
+            line.Append('\''); line.Append(escape(weather.Value.Name)); line.Append('\'');
             if (rate == 100) break;
             line.Append(", "); line.Append(rate); line.Append(", ");
         }
@@ -46,7 +47,7 @@ var weatherUsed = new HashSet<uint>();
 File.WriteAllLines("weathers.txt", weathers.Keys.Cast<string>());
 
 var langs = new StringBuilder();
-void extractLangs<T>(Func<T?, SeString?> getText, HashSet<uint> used) where T : ExcelRow
+void extractLangs<T>(Func<T?, ReadOnlySeString?> getText, HashSet<uint> used) where T : struct, IExcelRow<T>
 {
     var weatherNameSheets = new (string lang, ExcelSheet<T> sheet)[]
     {
@@ -57,11 +58,11 @@ void extractLangs<T>(Func<T?, SeString?> getText, HashSet<uint> used) where T : 
         ("zh", luminaCn.GetExcelSheet<T>()!),
         ("ko", lumina.GetExcelSheet<T>(Lumina.Data.Language.Korean)!),
     };
-    var count = weatherNameSheets[0].sheet.RowCount;
+    var count = weatherNameSheets[0].sheet.Count;
     for (var i = 0u; i < count; i++)
     {
         if (!used.Contains(i)) continue;
-        var names = weatherNameSheets.Select(x => (x.lang, name: escape(getText(x.sheet.GetRow(i))) ?? "")).ToArray();
+        var names = weatherNameSheets.Select(x => (x.lang, name: escape(getText(x.sheet.GetRowOrDefault(i))) ?? "")).ToArray();
         var key = names[0].name;
         if (string.IsNullOrEmpty(key)) continue;
 
